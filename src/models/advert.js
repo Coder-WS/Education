56b941e76622d2031b97f53a5ffc757a447eb82d
Coder-WS <wushuai686@163.com>
2020-03-22 11:14:34 +0800

    初始化项目

---
+++ src/models/advert.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb://localhost/edu')
+
+const advertSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  image: { type: String, required: true },
+  link: { type: String, required: true },
+  start_time: { type: Date, required: true },
+  end_time: { type: Date, required: true },
+  create_time: { type: Date, default: Date.now },
+  last_modified: { type: Date, default: Date.now }
+})
+
+
+const Advert = mongoose.model('Advert', advertSchema)
+export default Advert
+
+
+// pageSize 3
+// 1 0 3
+// 2 3 3
+// 3 6 3
+// n (n-1) * 3 3
+// 相当于MySQL中，使用SQL语句一样，这里mongoose将其封装成了方法。不用写SQL语句了。
+// Advert
+//   .find()
+//   .skip(3)
+//   .limit(3)
+//   .exec((err, result) => {
+//     if (err) {
+//       throw err
+//     }
+//     console.log(result)
+//   })
